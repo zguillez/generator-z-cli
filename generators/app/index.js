ae77a85b3b06a855c1fa5b3e3cef568378c41919
Guillermo de la Iglesia <zguillez@gmail.com>
2017-09-13 19:51:59 +0200

    base cli frameworks

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,35 +2,50 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
-
+const exec = require('child_process').exec;
+const spawn = require('child_process').spawn;
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the wondrous ' + chalk.red('generator-z-cli') + ' generator!'
-    ));
-
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
+    this.log(yosay('Welcome to the zetadelic ' + chalk.red('generator-z-cli') + ' generator!'));
+    /*const prompts = [{
+     type: 'confirm',
+     name: 'someAnswer',
+     message: 'Would you like to enable this option?',
+     default: true
+     }];*/
+    const prompts = [
+      {
+        type: 'text',
+        name: 'name',
+        message: 'Which is the app name?',
+        default: 'myapp'
+      }, {
+        type: 'list',
+        name: 'framework',
+        message: 'Which Javascript framework you want to use?',
+        choices: ['Angular', 'Ionic', 'Polymer', 'ReactJS', 'Vue']
+      }
+    ];
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
       this.props = props;
     });
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    this.fs.copyTpl(this.templatePath(`package-${this.props.framework.toLowerCase()}.json`), this.destinationPath('package.json'), {name: this.props.name});
+    if(this.props.framework === 'ReactJS') {
+      this.fs.copy(this.templatePath(`.eslintrc-${this.props.framework.toLowerCase()}`), this.destinationPath('.eslintrc'), {name: this.props.name});
+    }
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({
+      bower: false,
+      npm: false,
+      yarn: true,
+      callback: () => {
+        console.log(chalk.green('=> Everything is ready!'));
+      }
+    });
   }
 };
