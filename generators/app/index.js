c860dcb874a38431fdc6ea97b7342e6d5e4e2ba0
Guillermo de la Iglesia <zguillez@gmail.com>
2017-09-18 12:29:58 +0200

    update reactjs-cli

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,11 +2,12 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
-const exec = require('child_process').exec;
-const spawn = require('child_process').spawn;
+const fs = require('fs');
+const path = require('path');
+const version = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../') + '/package.json')).version;
 module.exports = class extends Generator {
   prompting() {
-    this.log(yosay('Welcome to the zetadelic ' + chalk.red('generator-z-cli') + ' generator!'));
+    this.log(yosay('generator-z-cli ' + chalk.green(`v${version}`)));
     /*const prompts = [{
      type: 'confirm',
      name: 'someAnswer',
@@ -32,10 +33,13 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copyTpl(this.templatePath(`package-${this.props.framework.toLowerCase()}.json`), this.destinationPath('package.json'), {name: this.props.name});
+    this.fs.copyTpl(this.templatePath(`package-${this.props.framework.toLowerCase()}.json`), this.destinationPath(`package.json`), {name: this.props.name});
+    this.fs.copy(this.templatePath(`package-${this.props.framework.toLowerCase()}.prod.json`), this.destinationPath(`package.prod.json`));
     if(this.props.framework === 'ReactJS') {
-      this.fs.copy(this.templatePath(`.eslintrc-${this.props.framework.toLowerCase()}`), this.destinationPath('.eslintrc'), {name: this.props.name});
+      this.fs.copy(this.templatePath(`.eslintrc-${this.props.framework.toLowerCase()}`), this.destinationPath(`.eslintrc`));
     }
+    this.fs.copy(this.templatePath(`.sshconfig`), this.destinationPath(`.sshconfig`));
+    this.fs.copy(this.templatePath(`bin`), this.destinationPath(`bin`));
   }
 
   install() {
@@ -44,7 +48,7 @@ module.exports = class extends Generator {
       npm: false,
       yarn: true,
       callback: () => {
-        console.log(chalk.green('=> Everything is ready!'));
+        console.log(chalk.green(`=> Everything is ready!`));
       }
     });
   }
